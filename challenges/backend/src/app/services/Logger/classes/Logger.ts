3895ee2866e14f6b514f9cf74458c6d1e7610df4
Mohab Amr <mohabamr1@gmail.com>
2023-06-09 20:05:39 +0300

    feat: more logger functionality

--- challenges/backend/src/app/services/Logger/classes/Logger.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.ts
@@ -1,16 +1,22 @@
-import {ILogger} from "../interface/ILogger";
-import {injectable} from "inversify";
+import { ILogger } from "../interface/ILogger";
+import { injectable } from "inversify";
 import "reflect-metadata";
 
 @injectable()
 export class Logger implements ILogger {
-
-    public constructor() {
-    }
-
+    public constructor() {}
 
     public log(message: string): void {
         console.log(`[LOG]: ${message}`);
     }
 
+    public warn(message: string): void {
+        console.warn(`[WARN]: ${message}`);
+    }
+    public info(message: string): void {
+        console.info(`[INFO]: ${message}`);
+    }
+    public error(message: string): void {
+        console.error(`[ERROR]: ${message}`);
+    }
 }
