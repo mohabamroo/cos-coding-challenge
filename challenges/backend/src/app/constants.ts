e007dd22e807e03cbf6d17648de10a58e4096d35
Mohab Amr <mohabamr1@gmail.com>
2023-06-10 19:16:01 +0300

    feat: added HttpClient (+ axios)

---
+++ challenges/backend/src/app/constants.ts
@@ -0,0 +1,1 @@
+export const COS_API_ROOT = "https://api-core-dev.caronsale.de/api";
---
+++ challenges/backend/src/app/services/HttpClient/interface/IHttpClient.ts
@@ -0,0 +1,12 @@
+export interface HttpConfig {
+    headers?: { [key: string]: string };
+    params?: { [key: string]: string };
+    timeout?: number;
+}
+export interface IHttpClient {
+    get(url: string, config?: HttpConfig): Promise<any>;
+    post(url: string, data?: any, config?: HttpConfig): Promise<any>;
+    put(url: string, data?: any, config?: HttpConfig): Promise<any>;
+    patch(url: string, data?: any, config?: HttpConfig): Promise<any>;
+    delete(url: string, config?: HttpConfig): Promise<any>;
+}
