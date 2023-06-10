0a59fc7baf223d293c48f0b0ef8bfd7f0bbb412a
Mohab Amr <mohabamr1@gmail.com>
2023-06-10 21:48:06 +0300

    test: proper mock of console in Logger test

--- challenges/backend/src/app/services/Logger/classes/Logger.spec.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.spec.ts
@@ -6,33 +6,33 @@ describe("Logger Tests", () => {
     it("should logs a message in console", async () => {
         const logger = new Logger();
         const message = "test";
-        let spy = sinon.spy(console, "log");
+        let stub = sinon.stub(console, "log");
         logger.log(message);
-        assert(spy.calledWith(`[LOG]: ${message}`));
-        spy.restore();
+        assert(stub.calledWith(`[LOG]: ${message}`));
+        stub.restore();
     });
     it("should logs an error message in console", async () => {
         const logger = new Logger();
         const message = "test";
-        let spy = sinon.spy(console, "error");
+        let stub = sinon.stub(console, "error");
         logger.error(message);
-        assert(spy.calledWith(`[ERROR]: ${message}`));
-        spy.restore();
+        assert(stub.calledWith(`[ERROR]: ${message}`));
+        stub.restore();
     });
     it("should logs a warning message in console", async () => {
         const logger = new Logger();
         const message = "test";
-        let spy = sinon.spy(console, "warn");
+        let stub = sinon.stub(console, "warn");
         logger.warn(message);
-        assert(spy.calledWith(`[WARN]: ${message}`));
-        spy.restore();
+        assert(stub.calledWith(`[WARN]: ${message}`));
+        stub.restore();
     });
     it("should logs an info message in console", async () => {
         const logger = new Logger();
         const message = "test";
-        let spy = sinon.spy(console, "info");
+        let stub = sinon.stub(console, "info");
         logger.info(message);
-        assert(spy.calledWith(`[INFO]: ${message}`));
-        spy.restore();
+        assert(stub.calledWith(`[INFO]: ${message}`));
+        stub.restore();
     });
 });
